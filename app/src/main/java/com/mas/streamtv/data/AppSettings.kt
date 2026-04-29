543733dbc342a095fdad4ccc12f894dcaf6cabd3
Black MIrror <160822257+pentonic0@users.noreply.github.com>
2026-04-29 15:58:58 +0600

    Delete app directory

--- app/src/main/java/com/mas/streamtv/data/AppSettings.kt
+++
@@ -1,9 +0,0 @@
-package com.mas.streamplayer.data
-
-data class AppSettings(
-    val autoPip: Boolean = false,
-    val landscapeMode: Boolean = false,
-    val resumePlaying: Boolean = false,
-    val seekDurationSeconds: Int = 10,
-    val startAsMute: Boolean = false
-)
